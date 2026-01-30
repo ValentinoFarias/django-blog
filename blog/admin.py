881e5e01168ee85c1c2a2521b845415cbe7bde04
ValentinoFarias <valentinofariascarrion@gmail.com>
2026-01-30 12:04:31 +0000

    Adding comments section

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,13 @@
+from xml.etree.ElementTree import Comment
 from django.contrib import admin
+from .models import Post
+from .models import Comment
 
 # Register your models here.
 
-from .models import Post
+
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
+
