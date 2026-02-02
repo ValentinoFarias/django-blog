9ccddc8fdbc62b27c400fe023b6e53c3e98d8f04
ValentinoFarias <valentinofariascarrion@gmail.com>
2026-02-02 12:29:40 +0000

    Working with postdetail

--- codestar/urls.py
+++ codestar/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
