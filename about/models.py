26d87ed9aa672d1a63d20cf2df28e93f88280264
ValentinoFarias <valentinofariascarrion@gmail.com>
2026-02-03 13:31:42 +0000

    Fixing about section

--- about/models.py
+++ about/models.py
@@ -1,26 +1,22 @@
 from django.db import models
 
+# Create your models here.
+
 
 class About(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
-    class Meta:
-        ordering = ["-updated_on"]
-
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
 
 
-class Collaborate(models.Model):
-    name = models.CharField(max_length=80)
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ["-created_on"]
-
-    def __str__(self) -> str:
-        return f"{self.name} ({self.email})"
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
